e083bd0a198d9be36df13e680c2a286356bfc7b1
VimVoyager <86576612+VimVoyager@users.noreply.github.com>
2025-12-18 22:13:16 +0000

    Remove redundant HTML from navbar component

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -18,18 +18,18 @@
 	class="relative bg-white shadow-sm dark:bg-black dark:shadow-none dark:after:pointer-events-none dark:after:absolute dark:after:inset-x-0 dark:after:bottom-0 dark:after:h-px dark:after:bg-white/10"
 >
 	<div class="mx-auto px-2 sm:px-4 lg:px-8">
-		<div class="flex h-16 justify-between">
+		<div class="flex h-14 justify-between">
 			<div class="flex px-2 lg:px-0">
 				<div class="flex shrink-0 items-center">
 					<img
-						src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=red&shade=600"
+						src="src/lib/assets/streaming-logo.png"
 						alt="Your Company"
-						class="h-8 w-auto dark:hidden"
+						class="h-11 w-auto dark:hidden"
 					/>
 					<img
-						src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=red&shade=500"
+						src="src/lib/assets/streaming-logo.png"
 						alt="Your Company"
-						class="h-8 w-auto not-dark:hidden"
+						class="h-11 w-auto not-dark:hidden"
 					/>
 				</div>
 			</div>
@@ -96,147 +96,6 @@
 					</svg>
 				</button>
 			</div>
-			<div class="hidden lg:ml-4 lg:flex lg:items-center">
-				<button
-					type="button"
-					class="relative shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-2 focus:outline-offset-2 focus:outline-red-600 dark:hover:text-white dark:focus:outline-red-500"
-				>
-					<span class="absolute -inset-1.5"></span>
-					<span class="sr-only">View notifications</span>
-					<svg
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="1.5"
-						data-slot="icon"
-						aria-hidden="true"
-						class="size-6"
-					>
-						<path
-							d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-					</svg>
-				</button>
-
-				<!-- Profile dropdown -->
-				<el-dropdown class="relative ml-4 shrink-0">
-					<button
-						class="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 dark:focus-visible:outline-red-500"
-					>
-						<span class="absolute -inset-1.5"></span>
-						<span class="sr-only">Open user menu</span>
-						<img
-							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-							alt=""
-							class="size-8 rounded-full bg-gray-100 outline -outline-offset-1 outline-black/5 dark:bg-gray-800 dark:outline-white/10"
-						/>
-					</button>
-
-					<el-menu
-						anchor="bottom end"
-						popover
-						class="w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline-1 outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
-					>
-						<a
-							href="#"
-							class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden dark:text-gray-300 dark:focus:bg-white/5"
-							>Your profile</a
-						>
-						<a
-							href="#"
-							class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden dark:text-gray-300 dark:focus:bg-white/5"
-							>Settings</a
-						>
-						<a
-							href="#"
-							class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden dark:text-gray-300 dark:focus:bg-white/5"
-							>Sign out</a
-						>
-					</el-menu>
-				</el-dropdown>
-			</div>
 		</div>
 	</div>
-
-	<el-disclosure id="mobile-menu" hidden class="block lg:hidden">
-		<div class="space-y-1 pt-2 pb-3">
-			<!-- Current: "bg-indigo-50 border-indigo-600 text-indigo-700 dark:border-indigo-500 dark:bg-indigo-600/10 dark:text-indigo-400", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white" -->
-			<a
-				href="#"
-				class="block border-l-4 border-indigo-600 bg-indigo-50 py-2 pr-4 pl-3 text-base font-medium text-indigo-700 dark:border-indigo-500 dark:bg-indigo-600/10 dark:text-indigo-400"
-				>Dashboard</a
-			>
-			<a
-				href="#"
-				class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
-				>Team</a
-			>
-			<a
-				href="#"
-				class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
-				>Projects</a
-			>
-			<a
-				href="#"
-				class="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
-				>Calendar</a
-			>
-		</div>
-		<div class="border-t border-gray-200 pt-4 pb-3 dark:border-white/10">
-			<div class="flex items-center px-4">
-				<div class="shrink-0">
-					<img
-						src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-						alt=""
-						class="size-10 rounded-full bg-gray-100 outline -outline-offset-1 outline-black/5 dark:bg-gray-800 dark:outline-white/10"
-					/>
-				</div>
-				<div class="ml-3">
-					<div class="text-base font-medium text-gray-800 dark:text-gray-200">Tom Cook</div>
-					<div class="text-sm font-medium text-gray-500 dark:text-gray-400">tom@example.com</div>
-				</div>
-				<button
-					type="button"
-					class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-2 focus:outline-offset-2 dark:hover:text-white"
-				>
-					<span class="absolute -inset-1.5"></span>
-					<span class="sr-only">View notifications</span>
-					<svg
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="1.5"
-						data-slot="icon"
-						aria-hidden="true"
-						class="size-6"
-					>
-						<path
-							d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-						/>
-					</svg>
-				</button>
-			</div>
-			<div class="mt-3 space-y-1">
-				<a
-					href="#"
-					class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white"
-					>Your profile</a
-				>
-				<a
-					href="#"
-					class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white"
-					>Settings</a
-				>
-				<a
-					href="#"
-					class="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-white"
-					>Sign out</a
-				>
-			</div>
-		</div>
-	</el-disclosure>
 </nav>
