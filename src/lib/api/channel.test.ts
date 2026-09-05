778aa1efcbe11cb5df0a7186ce971166fc7827cd
VimVoyager <86576612+VimVoyager@users.noreply.github.com>
2026-09-05 14:39:21 +0100

    test: implement tests for channel API fetching

---
+++ src/lib/api/channel.test.ts
@@ -0,0 +1,36 @@
+import { describeJsonFetcher } from '../../tests/helpers/describeJsonFetcher';
+import { getChannelInfo, getChannelVideos, getChannelVideosNextPage } from '$lib/api/channel';
+import channelDetailsApiResponseFixture from '../../tests/fixtures/api/channelDetailsApiResponse.json'
+import channelVideosApiResponseFixture from '../../tests/fixtures/api/channelVideosApiResponse.json'
+
+describeJsonFetcher({
+	name: 'getChannelInfo',
+	call: getChannelInfo,
+	endpoint: '/channels',
+	idParam: 'id',
+	fixture: channelDetailsApiResponseFixture
+})
+
+describeJsonFetcher({
+	name: 'getChannelVideos',
+	call: getChannelVideos,
+	endpoint: '/channels/tab',
+	idParam: 'id',
+	fixture: channelVideosApiResponseFixture
+});
+
+describeJsonFetcher({
+	name: 'getChannelVideosNextPage',
+	call: (channelId: string, fetchFn) =>
+		getChannelVideosNextPage(
+			channelId,
+			'GLITCH',
+			'https://www.youtube.com/youtubei/v1/browse?prettyPrint=false',
+			'EqADEgNsdHQamANTQ2lDQVF0TVdVMTJTbGMxY2xsTlJZSUJDMjlsY1ZWSVJYQTB',
+			'VERIFIED',
+			fetchFn
+		),
+	endpoint: '/channels/tab/page',
+	idParam: 'channelId',
+	fixture: channelDetailsApiResponseFixture
+})
